e85d731b1ea9b1711ab3cf6e7c5087bc99374418
Dmitry <89202929892@mail.ru>
2023-12-14 00:47:27 +0300

    Добавил к станици гардин fancybox

--- templates/technology/headHiddenCurtain.php
+++ templates/technology/headHiddenCurtain.php
@@ -1,5 +1,8 @@
 
 	<title>Ниши и гардины для натяжных потолков</title>
 	<link rel="stylesheet" type="text/css" href="../css/technology/hiddenCurtainStyle.css">
+	<link rel="stylesheet" type="text/css" href="../css/fancybox.css">
+	<script defer type="text/javascript" src="../js/fancybox.js"></script>
+	<script defer type="text/javascript" src="../js/hidenCurtain.js"></script>
 	<meta name="description" content="Скрытая гардина со специальным профилем ПК-5. Скрытая гардина более бюджетный вариант. Потолочная гардина с закруглениями и блендой. Варианты исполнения, подсветка для гардины, примеры работ, цены. Заказать скрытую гардину для натяжного потолка."/>
 </head>
--- templates/technology/hiddenCurtainPage.php
+++ templates/technology/hiddenCurtainPage.php
@@ -10,10 +10,38 @@
 <div class="vari_gard_cont">
 	<div class="vari_gard_item">
 		<p class="parag_color_text">С Профилем Lumfer</p>
-		<picture class="vari_cont_picture">
+		<picture data-fancybox="gallery1" data-caption="Профиль Lumfer" data-src="../img/hiddenCurtainPage/jpg/sfsdsgdht3h3tn.jpg" class="vari_cont_picture">
 			<source srcset='../img/hiddenCurtainPage/webp/sfsdsgdht3h3tn.webp' type="image/webp">
 			<img src='../img/hiddenCurtainPage/jpg/sfsdsgdht3h3tn.jpg'  alt="Скрытая гардина с профилем Lumfer">
 		</picture>
+		<div class="cont_img_prev">
+			<a data-fancybox="gallery1" data-caption="Профиль Lumfer" href="../img/hiddenCurtainPage/lumf/324324234.jpg">
+				<img alt="Скрытая гардина с профилем Lumfer" src="../img/hiddenCurtainPage/lumf/trumb/13dsffwtrumb.jpg">
+			</a>
+			<a data-fancybox="gallery1" data-caption="Профиль Lumfer" href="../img/hiddenCurtainPage/lumf/fkfsff732.jpg">
+				<img alt="Скрытая гардина с профилем Lumfer" src="../img/hiddenCurtainPage/lumf/trumb/dlsdfltrumb.jpg">
+			</a>
+			<a data-fancybox="gallery1" data-caption="Профиль Lumfer" href="../img/hiddenCurtainPage/lumf/ghkdkhghdg.jpg">
+				<img alt="Скрытая гардина с профилем Lumfer" src="../img/hiddenCurtainPage/lumf/trumb/hgkgtrumb.jpg">
+			</a>
+			<a data-fancybox="gallery1" data-caption="Профиль Lumfer" href="../img/hiddenCurtainPage/lumf/c024fbdde229b07041d17faf317e02bf.jpg">
+				<img alt="Скрытая гардина с профилем Lumfer" src="../img/hiddenCurtainPage/lumf/trumb/c024fbdde229b07041d17faf317e02bftrumb.jpg">
+			</a>
+		</div>
+		<div class="cont_img_prev">
+			<a data-fancybox="gallery1" data-caption="Профиль Lumfer" href="../img/hiddenCurtainPage/lumf/73e04859b069af9faf89c4627450884b.jpg">
+				<img alt="Скрытая гардина с профилем Lumfer" src="../img/hiddenCurtainPage/lumf/trumb/73e04859b069af9faf89c4627450884trumb.jpg">
+			</a>
+			<a data-fancybox="gallery1" data-caption="Профиль Lumfer" href="../img/hiddenCurtainPage/lumf/5875b135dc5db01a29f6c6cb69919dc7.jpg">
+				<img alt="Скрытая гардина с профилем Lumfer" src="../img/hiddenCurtainPage/lumf/trumb/5875b135dc5db01a29f6c6cb69919dc7trumb.jpg">
+			</a>
+			<a data-fancybox="gallery1" data-caption="Профиль Lumfer" href="../img/hiddenCurtainPage/lumf/894011c5ec9f624b37db7db19a918a21.jpg">
+				<img alt="Скрытая гардина с профилем Lumfer" src="../img/hiddenCurtainPage/lumf/trumb/894011c5ec9f624b37db7db19a918a21trumb.jpg">
+			</a>
+			<a data-fancybox="gallery1" data-caption="Профиль Lumfer" href="../img/hiddenCurtainPage/lumf/5a483738b426564fd1884b5b7e323053.jpg">
+				<img alt="Скрытая гардина с профилем Lumfer" src="../img/hiddenCurtainPage/lumf/trumb/5a483738b426564fd1884b5b7e323053trumb.jpg">
+			</a>
+		</div>
 		<div class="vari_item_fexbox parag_color_text">
 			<span>
 				<div class="big_green_defice"></div><span>Возможно сделать обрыв гардины в любом месте</span>
@@ -29,10 +57,25 @@
 	</div>
 	<div class="vari_gard_item">
 		<p class="parag_color_text">С Профилем ПК-5</p>
-		<picture class="vari_cont_picture">
+		<picture data-fancybox="gallery2" data-caption="Профиль ПК-5" data-src='<?php echo $GLOBALS["root_dir"]."img/rendered/jpg/nvowofbwbwb.jpg"?>' class="vari_cont_picture">
 			<source srcset='<?php echo $GLOBALS["root_dir"]."img/rendered/webp/nvowofbwbwb.webp"?>' type="image/webp">
 			<img src='<?php echo $GLOBALS["root_dir"]."img/rendered/jpg/nvowofbwbwb.jpg"?>'  alt="Скрытая гардина с профилем ПК-5">
 		</picture>
+		<div class="cont_img_prev">
+			<a data-fancybox="gallery2" data-caption="Профиль ПК-5" href="../img/hiddenCurtainPage/pk5/2e2536653a7872cb91496cc87be15ec8.jpg">
+				<img alt="Скрытая гардина с профилем ПК-5" src="../img/hiddenCurtainPage/pk5/trumb/2e2536653a7872cb91496cc87be15ec8trumb.jpg">
+			</a>
+			<a data-fancybox="gallery2" data-caption="Профиль ПК-5" href="../img/hiddenCurtainPage/pk5/255a317df745880e5e3bb148183eb0c4.jpg">
+				<img alt="Скрытая гардина с профилем ПК-5" src="../img/hiddenCurtainPage/pk5/trumb/255a317df745880e5e3bb148183eb0c4trumb.jpg">
+			</a>
+			<a data-fancybox="gallery2" data-caption="Профиль ПК-5" href="../img/hiddenCurtainPage/pk5/906963cbd7bb65b8626c76926ed7222d.jpg">
+				<img alt="Скрытая гардина с профилем ПК-5" src="../img/hiddenCurtainPage/pk5/trumb/906963cbd7bb65b8626c76926ed7222dtrumb.jpg">
+			</a>
+			<a data-fancybox="gallery2" data-caption="Профиль ПК-5" href="../img/hiddenCurtainPage/pk5/maxresdefault.jpg">
+				<img alt="Скрытая гардина с профилем ПК-5" src="../img/hiddenCurtainPage/pk5/maxresdefault.jpg">
+			</a>
+			
+		</div>
 		<div class="vari_item_fexbox parag_color_text">
 			<span>
 				<div class="big_green_defice"></div><span>Не требуется финишная отдела перекрытия</span>
@@ -52,10 +95,26 @@
 <div class="vari_gard_cont">
 	<div class="vari_gard_item">
 		<p class="parag_color_text">Имитация стены</p>
-		<picture class="vari_cont_picture">
+		<picture data-fancybox="gallery3" data-caption="Имитация стены" data-src='<?php echo $GLOBALS["root_dir"]."img/rendered/jpg/nnkdnwnkvwiiv.jpg"?>' class="vari_cont_picture">
 			<source srcset='<?php echo $GLOBALS["root_dir"]."img/rendered/webp/nnkdnwnkvwiiv.webp"?>' type="image/webp">
-			<img src='<?php echo $GLOBALS["root_dir"]."img/rendered/jpg/nnkdnwnkvwiiv.jpg"?>'  alt="Скрытая гардина с профилем ПК-5">
+			<img src='<?php echo $GLOBALS["root_dir"]."img/rendered/jpg/nnkdnwnkvwiiv.jpg"?>'  alt="Фото имитаци стены">
 		</picture>
+		<div class="cont_img_prev">
+			<a data-fancybox="gallery3" data-caption="Имитация стены" href="../img/hiddenCurtainPage/imitation/392fca89d42a8a595536388852386b3c.jpg">
+				<img alt="Фото имитаци стены" src="../img/hiddenCurtainPage/imitation/trumb/392fca89d42a8a595536388852386b3ctrumb.jpg">
+			</a>
+			<a data-fancybox="gallery3" data-caption="Имитация стены" href="../img/hiddenCurtainPage/imitation/265835a312428762df210a7b583c87ab.jpg">
+				<img alt="Фото имитаци стены" src="../img/hiddenCurtainPage/imitation/trumb/265835a312428762df210a7b583c87abtrumb.jpg">
+			</a>
+			<a data-fancybox="gallery3" data-caption="Имитация стены" href="../img/hiddenCurtainPage/imitation/c18a830d631fac25504fcee9aef35665.jpg">
+				<img alt="Фото имитаци стены" src="../img/hiddenCurtainPage/imitation/trumb/c18a830d631fac25504fcee9aef35665.jpg">
+			</a>
+			<a data-fancybox="gallery3" data-caption="Имитация стены" href="../img/hiddenCurtainPage/imitation/e4da2af5ce295d66743c013b25f48476.jpg">
+				<img alt="Фото имитаци стены" src="../img/hiddenCurtainPage/imitation/trumb/e4da2af5ce295d66743c013b25f48476trumb.jpg">
+			</a>
+			
+			
+		</div>
 		<div class="vari_item_fexbox parag_color_text">
 			<span>
 				<div class="big_green_defice"></div><span>Видны элементы конструкции со стороны окна</span>
@@ -72,7 +131,7 @@
 		<p class="parag_color_text">С перегибом полотна</p>
 		<picture class="vari_cont_picture">
 			<source srcset="../img/hiddenCurtainPage/webp/df9bd9b6-b7b3-0f8d-7a91-ba4ca4d509c5.webp" type="image/webp">
-			<img src="../img/hiddenCurtainPage/jpg/df9bd9b6-b7b3-0f8d-7a91-ba4ca4d509c5.jpg"  alt="Скрытая гардина с профилем ПК-5">
+			<img src="../img/hiddenCurtainPage/jpg/df9bd9b6-b7b3-0f8d-7a91-ba4ca4d509c5.jpg"  alt="Скрытая гардина с перегибом">
 		</picture>
 		<div class="vari_item_fexbox parag_color_text">
 			<span>
@@ -88,12 +147,53 @@
 		</div>
 	</div>
 </div>
+
+<div style="display: flex; justify-content: center; margin-top: 50px;">
+	<div class="vert_line"></div>
+</div>
+<div class="heder_present" style="position: relative; z-index: 2;">
+	<span>Еще варианты</span>
+</div>
+
 <div class="vari_gard_cont">
+
+	<div class="vari_gard_item">
+		<p class="parag_color_text">С подсветкой</p>
+		<picture data-fancybox="gallery4" data-caption="Lumfer с подсветкой" data-src='../img/hiddenCurtainPage/lightdekor/dfsllfssalala.jpg' class="vari_cont_picture">
+			<img src="../img/hiddenCurtainPage/lightdekor/dfsllfssalala.jpg"  alt="Гардина с подсветкой">
+		</picture>
+		<div class="cont_img_prev">
+			<a data-fancybox="gallery4" data-caption="Имитация стены с подсветкой" href="../img/hiddenCurtainPage/lightdekor/e040926c931f2417759cf2299fcd4461.jpg">
+				<img alt="Гардина с подсветкой" src="../img/hiddenCurtainPage/lightdekor/trumb/e040926c931f2417759cf2299fcd4461trumb.jpg">
+			</a>
+			<a data-fancybox="gallery4" data-caption="ПК-5 с подсветкой" href="../img/hiddenCurtainPage/lightdekor/49032e8ca1cfe7b12168cb944fb385ae.jpeg">
+				<img alt="Гардина с подсветкой" src="../img/hiddenCurtainPage/lightdekor/49032e8ca1cfe7b12168cb944fb385ae.jpeg">
+			</a>
+			<a data-fancybox="gallery4" data-caption="Имитация стены с подсветкой" href="../img/hiddenCurtainPage/lightdekor/fkhskhsfsfsg.jpg">
+				<img alt="Гардина с подсветкой" src="../img/hiddenCurtainPage/lightdekor/fkhskhsfsfsg.jpg">
+			</a>
+			<a data-fancybox="gallery4" data-caption="Имитация стены с подсветкой" href="../img/hiddenCurtainPage/lightdekor/bkkfkfygkkw.jpg">
+				<img alt="Гардина с подсветкой" src="../img/hiddenCurtainPage/lightdekor/bkkfkfygkkw.jpg">
+			</a>		
+		</div>
+		<div class="vari_item_fexbox parag_color_text">
+			<span>
+				<div class="big_green_defice"></div><span>Возможна установка подсветки под любой тип гардины</span>
+			</span>
+			<span>
+				<div class="big_green_defice"></div> <span>Есть варианты установки умной посведки, а также обычных RGB и моно. </span>
+			</span>
+			<span>
+				<div class="big_green_defice"></div><span>Переходы подсветки со стены на гардину</span>
+			</span>
+		</div>
+	</div>
+
 	<div class="vari_gard_item">
 		<p class="parag_color_text">Накладной монтаж</p>
 		<picture class="vari_cont_picture">
 			<source srcset="../img/hiddenCurtainPage/webp/01ffwefw.webp" type="image/webp">
-			<img src="../img/hiddenCurtainPage/jpg/01ffwefw.jpg"  alt="Скрытая гардина с профилем ПК-5">
+			<img src="../img/hiddenCurtainPage/jpg/01ffwefw.jpg"  alt="Потолочная гардина">
 		</picture>
 		<div class="vari_item_fexbox parag_color_text">
 			<span>
